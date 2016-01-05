f86e7f7d41a38a7139b1668f3ca9d41a3e1df555
erick <erick>
2016-01-04 17:37:18 -0800

    Class and Transform use

---
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+# CAPTURE_CASH_AMOUNT = Transform(/^(£|\$|€)(\d+)$/) do |currency_symbol, amount|
+# Currency::Money.new(amount, currency_symbol)
+# end
+
+CAPTURE_CASH_AMOUNT = Transform(/^\$(\d+)$/) do |amount|
+amount.to_i
+end
+
+Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my super account$/) do |amount|
+	my_account = Account.new
+	my_account.deposit(amount)
+	my_account.balance.should eq(amount) , "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
+
+When(/^I request \$(\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^\$(\d+) should be dispensed$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
