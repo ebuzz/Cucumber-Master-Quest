5fa56cc683520ea4773c8e7a8c63531ac9fea2e5
erick <erick>
2016-01-05 11:12:08 -0800

    Cambios a los archivos para funcionar con nueva estructura

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -1,24 +1,16 @@
 # encoding: utf-8
 
-# CAPTURE_CASH_AMOUNT = Transform(/^(£|\$|€)(\d+)$/) do |currency_symbol, amount|
-# Currency::Money.new(amount, currency_symbol)
-# end
-
-CAPTURE_CASH_AMOUNT = Transform(/^\$(\d+)$/) do |amount|
-amount.to_i
-end
-
 Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my super account$/) do |amount|
-	my_account = Account.new
+	# my_account = Account.new
 	my_account.deposit(amount)
 	my_account.balance.should eq(amount) , "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
-  teller = Teller.new
+  # teller = Teller.new(cash_slot)
   teller.withdraw_from(my_account,amount)
 end
 
-Then(/^\$(\d+) should be dispensed$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
+  cash_slot.contents.should == amount
 end
